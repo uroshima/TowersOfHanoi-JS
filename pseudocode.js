60e49c7c87530f8e4b6849cb0d9c8db380f8870c
urosh <urosh.stojkovikj@gmail.com>
2018-10-09 11:51:15 -0700

    first commit

---
+++ pseudocode.js
@@ -0,0 +1,9 @@
+const run = function() => {
+  // 1.until all the rings are in an order (starting with the
+  // biggest one at the bottom and smallest at the top) on one of the
+  // towers (other than the starting one)
+  // 2.get a move from the player
+  // 3.make the move if its valid or return error if its not 
+  //
+}
+witch current player
