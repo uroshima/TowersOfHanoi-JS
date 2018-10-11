831719fdf8f3092988aa6479c446e4c33ce761b4
urosh <urosh.stojkovikj@gmail.com>
2018-10-10 22:23:49 -0700

    complete the game

--- game.js
+++ game.js
@@ -1,47 +1,65 @@
 class Game {
   constructor() {
-    this.stacks = [[3, 2, 1], [], []];
-    this.promptMove();
+    this.towers = [[3, 2, 1], [], []];
   }
 
-  promptMove() {
-    console.log(this.stacks);
-    this.startTowerIdx = prompt("What tower you wanna take a disc from");
-    this.endTowerIdx = prompt("What tower you wanna put the disc on?");
-    console.log(`take from tower ${this.startTowerIdx} and place it on tower ${this.endTowerIdx}`);
+  promptMove(reader, callback) {
+    this.print();
+    reader.question("Enter a starting tower: ", start => {
+      const startTowerIdx = parseInt(start);
+      reader.question("Enter an ending tower: ", end => {
+        const endTowerIdx = parseInt(end);
+        callback(startTowerIdx, endTowerIdx);
+      });
+    });
+  }
 
+  print() {
+    console.log(JSON.stringify(this.towers));
   }
 
   isValidMove(startTowerIdx, endTowerIdx) {
-   this.startTowerIdx = startTowerIdx - 1;
-   this.endTowerIdx = endTowerIdx - 1;
-   let error = new Error("Invalid move");
-   if ((this.startTowerIdx > 3 && this.startTowerIdx < 1) && (this.endTowerIdx > 3 && this.endTowerIdx < 1)) {
-     console.log(error);
-   } else if (!this.stacks[this.startTowerIdx][-1])
-     console.log(error);
-    else if (this.stacks[this.startTowerIdx][-1] > this.stack[this.endTowerIdx][-1])
-     console.log(error);
-    else
-     return true;
- }
+   const startTower = this.towers[startTowerIdx];
+   const endTower = this.towers[endTowerIdx];
+
+   if (startTower.length === 0) {
+      return false;
+    } else if (endTower.length == 0) {
+      return true;
+    } else {
+      const topStartDisc = startTower[startTower.length - 1];
+      const topEndDisc = endTower[endTower.length - 1];
+      return topStartDisc < topEndDisc;
+    }
+  }
+
+  move(startTowerIdx, endTowerIdx) {
+    if (this.isValidMove(startTowerIdx, endTowerIdx)) {
+      this.towers[endTowerIdx].push(this.towers[startTowerIdx].pop());
+      return true;
+    }
+    return false;
+  }
+
+  isWon() {
+    return (this.towers[1].length == 3) || (this.towers[2].length == 3)
+  }
+
+  run(reader, completionCallback) {
+    this.promptMove(reader, (startTowerIdx, endTowerIdx) => {
+      if (!this.move(startTowerIdx, endTowerIdx)) {
+        console.log("Invalid move!");
+      }
+      if (!this.isWon()) {
+        // Continue to play!
+        this.run(reader, completionCallback);
+      } else {
+        this.print();
+        console.log("You win!");
+        completionCallback();
+      }
+    });  
+  }
 }
 
 module.exports = Game;
-
-// var favDrink = prompt("What's your favorite cocktail drink?");
-// switch(favDrink) {
-//     case "Martini":
-//         text = "Excellent choice! Martini is good for your soul.";
-//         break;
-//     case "Daiquiri":
-//         text = "Daiquiri is my favorite too!";
-//         break;
-//     case "Cosmopolitan":
-//         text = "Really? Are you sure the Cosmopolitan is your favorite?";
-//         break;
-//     default:
-//         text = "I have never heard of that one..";
-//         break;
-// }
-// module.exports = Game;
--- playScript.js
+++ playScript.js
@@ -2,17 +2,34 @@ const Game = require("./game.js");
 const readline = require('readline');
 
 const reader = readline.createInterface({
-  // it's okay if this part is magic; it just says that we want to
-  // 1. output the prompt to the standard output (console)
-  // 2. read input from the standard input (again, console)
-
   input: process.stdin,
   output: process.stdout
 });
 
-let newGame = reader.question("You wanna play again? y/n", function (answer) {
-  return answer;
-});
-if (newGame == 'y') {
-  let game = new Game(reader, newGame);
-}
+let game = new Game();
+game.run(reader, completion);
+
+function completion() {
+  reader.question("Play again? y or n", restartGame => {
+    if (restartGame === "y") {
+      g = new Game();
+      g.run(reader, completion);
+    } else {
+      reader.close();
+    }
+  });
+};
+
+// this works too
+// function completion() {
+//   reader.question("Play again? y or n", helper(restartGame));
+// };
+//
+// function helper(restartGame) => {
+//   if (restartGame === "y") {
+//     g = new Game();
+//     g.run(reader, completion);
+//   } else {
+//     reader.close();
+//   }
+// }
